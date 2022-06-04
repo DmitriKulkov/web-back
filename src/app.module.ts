e6df13265c8c579ec64f2a05e6e228d62366b4f8
SimplePC <simplepc@pop-os.localdomain>
2022-06-04 10:58:10 +0300

    Fix bug with db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,40 +7,44 @@ import { Comments } from './comments/comments.entity';
 import { ConfigModule } from '@nestjs/config';
 import * as PostgresConnectionStringParser from 'pg-connection-string';
 
-const isProd = process.env.PGSSLMODE === 'production';
-const connectionOptions = isProd
-  ? PostgresConnectionStringParser.parse(process.env.DATABASE_URL)
-  : null;
+const getDBVars = () => {
+  const isProd = process.env.PGSSLMODE === 'production';
+  const connectionOptions = isProd
+    ? PostgresConnectionStringParser.parse(process.env.DATABASE_URL)
+    : null;
+  let res;
+  !isProd
+    ? (res = {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Post, Comments],
+      synchronize: true,
+      autoLoadEntities: true,
+      logging: true,
+    })
+    : (res = {
+      type: 'postgres',
+      host: connectionOptions.host,
+      port: parseInt(connectionOptions.port),
+      username: connectionOptions.user,
+      password: connectionOptions.password,
+      database: connectionOptions.database,
+      entities: [Post, Comments],
+      synchronize: true,
+      autoLoadEntities: true,
+      logging: true,
+    });
+  return res;
+};
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(
-      !isProd
-        ? {
-            type: 'postgres',
-            host: process.env.DB_HOST,
-            port: parseInt(process.env.DB_PORT),
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            entities: [Post, Comments],
-            synchronize: true,
-            autoLoadEntities: true,
-            logging: true,
-          }
-        : {
-            type: 'postgres',
-            host: connectionOptions.host,
-            port: parseInt(connectionOptions.port),
-            username: connectionOptions.user,
-            password: connectionOptions.password,
-            database: connectionOptions.database,
-            entities: [Post, Comments],
-            synchronize: true,
-            autoLoadEntities: true,
-            logging: true,
-          },
-    ),
+    TypeOrmModule.forRoot(getDBVars()),
     PostModule,
     CommentsModule,
   ],
